8885428943c755fdd10b6775efdc410b384c5d69
ma-gni <157604769+ma-gni@users.noreply.github.com>
2025-01-17 16:46:08 +0100

    adding the file of the project

---
+++ MinotOr/src/test/java/com/magnii/minotor/MinotOrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnii.minotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinotOrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
