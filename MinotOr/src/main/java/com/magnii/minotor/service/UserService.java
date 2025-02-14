156a230625378724479dd29f0584a64f05a3006f
ma-gni <157604769+ma-gni@users.noreply.github.com>
2025-02-14 01:59:24 +0100

    adding the service layer and the userservice

--- MinotOr/src/main/java/com/magnii/minotor/service/UserService.java
+++ MinotOr/src/main/java/com/magnii/minotor/service/UserService.java
@@ -1,4 +1,31 @@
 package com.magnii.minotor.service;
 
+import com.magnii.minotor.model.User;
+import com.magnii.minotor.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return Optional.ofNullable(userRepository.findByUsername(username));
+    }
+
+    @Transactional
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+
 }
