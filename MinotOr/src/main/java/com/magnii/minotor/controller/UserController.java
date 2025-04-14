539293b665d09acbcc92a123b2b61cc478ad841b
ma-gni <157604769+ma-gni@users.noreply.github.com>
2025-04-15 01:26:07 +0200

    finally last test everything should be up and running

--- MinotOr/src/main/java/com/magnii/minotor/controller/UserController.java
+++ MinotOr/src/main/java/com/magnii/minotor/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.magnii.minotor.controller;
 
+import com.magnii.minotor.dto.UserDTO;
+import com.magnii.minotor.mapper.UserMapper;
 import com.magnii.minotor.model.User;
 import com.magnii.minotor.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -7,40 +9,46 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers(){
-        List<User> users = userService.getUsers();
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getUsers()
+                .stream()
+                .map(userMapper::toDto)
+                .collect(Collectors.toList());
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{username}")
-    public ResponseEntity<User> getUserByUsername(@PathVariable String username){
+    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable String username) {
         Optional<User> userOpt = userService.getUserByUsername(username);
-        return userOpt.map(ResponseEntity::ok)
+        return userOpt.map(user -> ResponseEntity.ok(userMapper.toDto(user)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody User user){
+    public ResponseEntity<Void> createUser(@RequestBody User user) {
         userService.saveUser(user);
         return ResponseEntity.status(201).build();
     }
 
     @DeleteMapping("/{username}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String username){
+    public ResponseEntity<Void> deleteUser(@PathVariable String username) {
         Optional<User> userOpt = userService.getUserByUsername(username);
-        if(userOpt.isPresent()){
+        if (userOpt.isPresent()) {
             userService.deleteUser(userOpt.get());
             return ResponseEntity.ok().build();
         }
